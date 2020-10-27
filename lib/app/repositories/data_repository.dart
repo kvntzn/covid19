2adfe05e5de7348d0f04ddc8599cddf06a0e0836
heathkev <kevintuazon45@gmail.com>
2020-10-27 12:02:55 +0800

    handle token expire/refresh

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,13 +1,29 @@
 import 'package:covid19/app/services/api.dart';
 import 'package:covid19/app/services/api_service.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart';
 
-class DataRepository{
+class DataRepository {
   DataRepository({@required this.apiService});
   final APIService apiService;
 
+  String _accessToken;
+
   Future<int> getEndpointData(Endpoint endpoint) async {
-    final accessToken = await apiService.getAccessToken();
-    await apiService.getEndPointData(accessToken: accessToken, endpoint: endpoint)
+    try {
+      if (_accessToken == null) {
+        _accessToken = await apiService.getAccessToken();
+      }
+      return await apiService.getEndPointData(
+          accessToken: _accessToken, endpoint: endpoint);
+    } on Response catch (response) {
+      // if unauthorized, get access token
+      if (response.statusCode == 401) {
+        _accessToken = await apiService.getAccessToken();
+        return await apiService.getEndPointData(
+            accessToken: _accessToken, endpoint: endpoint);
+      }
+      rethrow;
+    }
   }
 }

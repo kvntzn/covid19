421aab5ea42825774a8cf69dd32dee25028b704e
heathkev <kevintuazon45@gmail.com>
2020-10-28 12:37:42 +0800

    added new method to get all endpoints data added endpoints data class

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,3 +1,4 @@
+import 'package:covid19/app/repositories/endpoints_data.dart';
 import 'package:covid19/app/services/api.dart';
 import 'package:covid19/app/services/api_service.dart';
 import 'package:flutter/cupertino.dart';
@@ -25,5 +26,29 @@ class DataRepository {
       }
       rethrow;
     }
+  }
+
+  Future<EndpointsData> _getAllEndpointsData() async {
+    final values = await Future.wait([
+      apiService.getEndPointData(
+          accessToken: _accessToken, endpoint: Endpoint.cases),
+      apiService.getEndPointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesSuspected),
+      apiService.getEndPointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesConfirmed),
+      apiService.getEndPointData(
+          accessToken: _accessToken, endpoint: Endpoint.deaths),
+      apiService.getEndPointData(
+          accessToken: _accessToken, endpoint: Endpoint.recovered)
+    ]);
+
+    return EndpointsData(
+      values: {
+      Endpoint.cases: values[0],
+      Endpoint.casesSuspected: values[1],
+      Endpoint.casesConfirmed: values[2],
+      Endpoint.deaths: values[3],
+      Endpoint.recovered: values[4]
+    });
   }
 }
